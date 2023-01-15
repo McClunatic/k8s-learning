32ed264ad6afdbad8cd22eaf199887a45024fec2
Brian McClune <bpmcclune@gmail.com>
2023-01-15 17:31:11 -0500

    Switch from networkd to netplan

--- wsl/setup-wsl.sh
+++ wsl/setup-wsl.sh
@@ -2,6 +2,5 @@
 set -x
 sed -e "s/HOSTNAME/$1/" $(dirname -- "$0")/templates/wsl.conf | sudo tee /etc/wsl.conf
 sudo chmod 644 /etc/wsl.conf 
-sed -e "s/IP_ADDRESS/$2/" $(dirname -- "$0")/templates/wsl-e.network | sudo tee /lib/systemd/network/wsl-e.network
-sudo chmod 644 /lib/systemd/network/wsl-e.network 
-sudo systemctl enable systemd-networkd
+sed -e "s/IP_ADDRESS/$2/" $(dirname -- "$0")/templates/netplan-config.yaml | sudo tee /etc/netplan/config.yaml
+sudo chmod 644 /etc/netplan/config.yaml
