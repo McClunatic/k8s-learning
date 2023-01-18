9ed66cece140383cf471b62d21fd6c5fc610b61a
Brian McClune <brian.mcclune@unnpp.gov>
2023-01-18 11:25:22 -0500

    Update resolved setup

--- wsl/setup-resolv.sh
+++ wsl/setup-resolv.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -x
-sed -e "s/NAMESERVER/$1/" $(dirname -- "$0")/templates/resolv.conf | sudo tee /etc/resolv.conf
-sudo chmod 644 /etc/resolv.conf
+sed -e "s/NAMESERVER/$1/" $(dirname -- "$0")/templates/resolved.conf | sudo tee /etc/systemd/resolved.conf
+sudo chmod 644 /etc/systemd/resolved.conf
 sudo systemctl enable systemd-resolved.service
+sudo systemctl restart systemd-resolved.service
