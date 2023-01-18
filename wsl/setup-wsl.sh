d4c4c799d87574d4c82ad90b734c35df17c260dc
Brian McClune <brian.mcclune@unnpp.gov>
2023-01-18 11:24:55 -0500

    Add /24 to IP_ADDRESS in setup-wsl

--- wsl/setup-wsl.sh
+++ wsl/setup-wsl.sh
@@ -2,5 +2,5 @@
 set -x
 sed -e "s/HOSTNAME/$1/" $(dirname -- "$0")/templates/wsl.conf | sudo tee /etc/wsl.conf
 sudo chmod 644 /etc/wsl.conf 
-sed -e "s/IP_ADDRESS/$2/" $(dirname -- "$0")/templates/netplan-config.yaml | sudo tee /etc/netplan/config.yaml
+sed -e "s/IP_ADDRESS/$2\/24/" $(dirname -- "$0")/templates/netplan-config.yaml | sudo tee /etc/netplan/config.yaml
 sudo chmod 644 /etc/netplan/config.yaml
